57c38cb996ccce761f2e113a3c98b44c0129bf5f
jonhayjr <jonhayjr@gmail.com>
2021-10-24 11:26:58 -0700

    Removed cached files

--- api/config/config.js
+++
@@ -1,8 +0,0 @@
-const connectionString = 'mongodb+srv://jonhay:h9ZcADD3WizZTZ3@cluster0.uiim1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-const DB = 'health-db';
-const collection = 'notes';
-module.exports = {
-     connectionString,
-     DB,
-     collection
-}
